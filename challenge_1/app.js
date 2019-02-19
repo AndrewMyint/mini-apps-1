d4a2435b0fd48b5085da679c6de1a60e6c7c44cd
Andrew Myint <doku.potharhtoo@gmail.com>
2019-02-19 00:56:38 -0500

    tictactoe interface added

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,9 @@
+console.log('Hello app.js');
 
+var row1 = document.getElementsByClassName("row1");
+console.log(row1.length)
 
+row1[0].addEventListener('click', function(event) {
+      console.log('Hello clicked');
+
+})
