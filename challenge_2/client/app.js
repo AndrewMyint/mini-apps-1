a4895ee28d8b1a863c8ffb692142dd0bdb24da26
Andrew Myint <doku.potharhtoo@gmail.com>
2019-02-23 16:28:16 -0500

    now the JSON object is correctly output in console.log

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,43 @@
 
+console.log("Hello from app.js")
+var connection = new XMLHttpRequest();
 
+// connection.open('GET', 'http://localhost:3000/hello', true);
+// connection.open('POST','http://localhost:3000/hello',true);
+// connection.onload = function() {
+//   if (connection.status === 200) {
+
+//     console.log('Thi is from onload ******** ' + connection.response);
+//   } else {
+//     console.log('wtf*******', connection.status)
+//   }
+// }
+// connection.send('Hello from client');
+// connection.send();
+// connection.onreadystatechange = function() {
+//   if (connection.readyState === 4 && connection.status === 200) {
+//     console.log("Hello from readystateChange", connection.response);
+//   }
+// }
+
+/*
+
+The server must flatten the JSON hierarchy,
+ mapping each item/object in the JSON to a single line of CSV report (see included sample output),
+where the keys of the JSON objects will be the columns of the CSV report.
+You may assume the JSON data has a regular structure and hierarchy (see included sample file).
+In other words, all sibling records at a particular level of
+the hierarchy will have the same set of properties,
+ but child objects might not contain the same properties. In all cases,
+ every property you encounter must be present in the final CSV output.
+You may also assume that child records in the JSON will always be in a property called `children`.
+*/
+
+// build the form
+// connect form data to the server
+// response the data to the client
+// build helper function or middleware for CSV report
+// response the formatted data to the client
+
+// think about how you can response report along with server
+// make it possible
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,42 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+// app.use((req, res, next) => {
+//   req.rawBody = '';
+//   // req.setEncoding('utf8');
+//   req.on('data', (chunk) => {
+//     req.rawBody += chunk;
+//  });
+//   req.on('end', function() {
+//     //  req.jsonBody = JSON.parse(req.rawBody);
+//      next();
+//    });
+// });
+app.set('client', path.join(__dirname + '/client'));
+app.set('view engine', 'ejs');
+app.use('/',express.static(path.join(__dirname + '/client')));
+app.use(bodyParser.urlencoded());
+// app.use(bodyParser.json());
+
+app.post('/upload_json', (req, res) => {
+  var temp = JSON.parse(req.body.json);
+  console.log('this is from upload_json******req.body ', temp);
+  // res.render('/client/index');
+  res.render('index', {jason: temp});
+  // res.send(`<p>${req.rawBody.json}<p>`);
+
+})
 
 
+// app.post('/hello', (req, res) => {
+//   console.log('This is from /hello request********* req.body', req.rawBody.json);
+//   res.send('This is from /hello response');
+//   // res.redirect('/hello');
+// });
+
+
+app.listen(4000, () => {
+  console.log('listening to port 3000');
+})
