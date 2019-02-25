425a962cad3e9d62627b72541a927ef6cb20f0e0
Andrew Myint <doku.potharhtoo@gmail.com>
2019-02-25 02:32:11 -0500

    before merging

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,18 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var file;
+$('#file').on('change', function(e) {
+  file = e.target.files[0];
+  console.log(file);
+})
+  // console.log('******',file);
   $('.submit').on('click', (e) => {
     e.preventDefault();
-    var json = $('#textarea').val();
+    // var formData = new FormData('#csv-Form');
+    // $.each($('#file')[0].files, (i, file)=> {
+    //   formData.append('file-' + i, file);
+    // })
+    var temp = JSON.stringify(file)
+    console.log(temp);
+    console.log(file)
+
     $.ajax({
-      url: 'http://localhost:4000/upload_json',
+      url: 'http://localhost:5000/upload_json',
       method: 'POST',
-      data: json,
+      data: JSON.stringify(file),
       dataType: 'html',
       contentType: 'text/plain',
-      success : function(html) {
-         console.log(html)
-        $('body').html(html);
+      processData: false,
+      success: function (html) {
+        console.log(html)
+        // $('body').html(html);
         // chaning the url
-        window.history.pushState("object or string", "Title", "/upload_json")
+        // window.history.pushState("object or string", "Title", "/upload_json")
       }
     })
   })
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' })
 const fs = require('fs');
 const ejs = require('ejs');
+// app.use(bodyParser.text());
 app.use(bodyParser.text());
-// app.use(bodyParser.urlencoded({extended: true}));
 app.set('client', path.join(__dirname + '/client'));
 //set the view engine to ejs so that don't need to type for extension
 // res.render() will loock in a view folder for the view
@@ -18,30 +18,34 @@ app.get('/', (req, res) => {
   res.render('/client/index.html');
 })
 // below is post request for file type input
-// app.post('/upload_json', upload.single('json'), (req, res) => {
-//   // req.body = req.body.json.slice(0, req.body.json.length - 1);
-//   // var temp = JSON.parse(req.body);
-//   fs.readFile(req.file.path, 'utf8',(err, data) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//     // req.body = req.body.json.slice(0, req.body.json.length - 1);
-//     // var temp = JSON.parse(req.body);
-//       var csv = formatToCSV(data);
-//       res.render('index', {json: csv})
-//     }
-//   })
-// })
 app.post('/upload_json', (req, res) => {
+  console.log('inside server post', req.file, req.body);
   // req.body = req.body.json.slice(0, req.body.json.length - 1);
   // var temp = JSON.parse(req.body);
- var csv = formatToCSV(req.body);
- var str;
-  res.render('index', {json: csv});
-  // str = ejs.render(csv, 'index', {json: csv});
-
-//  res.end();
+  // fs.readFile(req.body, 'utf8',(err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //   // req.body = req.body.json.slice(0, req.body.json.length - 1);
+  //   // var temp = JSON.parse(req.body);
+  //   console.log(data);
+  //     // var csv = formatToCSV(data);
+  //     // res.render('index', {json: csv})
+  //   }
+  // })
 })
+
+// ****** below is accepting value from textarea with ajax request
+// app.post('/upload_json', (req, res) => {
+//   // req.body = req.body.json.slice(0, req.body.json.length - 1);
+//   // var temp = JSON.parse(req.body);
+//  var csv = formatToCSV(req.body);
+//  var str;
+//   res.render('index', {json: csv});
+//   // str = ejs.render(csv, 'index', {json: csv});
+
+// //  res.end();
+// })
 
 // app.post('/hello', (req, res) => {
 //   console.log('This is from /hello request********* req.body', req.rawBody.json);
@@ -50,6 +54,6 @@ app.post('/upload_json', (req, res) => {
 // });
 
 
-app.listen(4000, () => {
-  console.log('listening to port 3000');
+app.listen(5000, () => {
+  console.log('listening to port 5000');
 })
